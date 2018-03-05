e4fe9e8c92c2b94173e7d62fdb81f59c7daa8249
Hearty Oh <heartyoh@hatiolab.com>
2018-03-05 11:57:03 +0900

    fix component template spec.

--- things-shell.config.js
+++ things-shell.config.js
@@ -1,36 +1,30 @@
 import locales from './locales'
 
 var templates = [{
-  name: 'billboard',
-  /* 다국어 키 표현을 어떻게.. */
-  description: '...',
-  /* 다국어 키 표현을 어떻게.. */
+  type: 'billboard',
+  description: 'Things Scene Billboard Chart Component',
+  icon: '',
   group: 'chartAndGauge',
   /* line|shape|textAndMedia|chartAndGauge|table|container|dataSource|IoT|3D|warehouse|form|etc */
-  icon: '../',
-  /* 또는, Object */
-  template: {
+  model: {
     type: 'billboard',
-    model: {
-      type: 'billboard',
-      top: 100,
-      left: 100,
-      width: 600,
-      height: 400,
-      fontSize: 10,
-      fillStyle: '#00ff00',
-      fontColor: '#FF0000',
-      strokeStyle: '#000',
-      lineWidth: 1,
-      data: [
-        ["data1", 30, 200, 100, 400, 150, 250],
-        ["data2", 250, 150, 120, 300, 200, 30]
-      ],
-      config: `
+    top: 100,
+    left: 100,
+    width: 600,
+    height: 400,
+    fontSize: 10,
+    fillStyle: '#00ff00',
+    fontColor: '#FF0000',
+    strokeStyle: '#000',
+    lineWidth: 1,
+    data: [
+      ["data1", 30, 200, 100, 400, 150, 250],
+      ["data2", 250, 150, 120, 300, 200, 30]
+    ],
+    config: `
 {
   type: "line"
 }`
-    }
   }
 }];
 
